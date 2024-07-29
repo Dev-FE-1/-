1a0d12856d33e6db5701f702dc088ac05e2c943c
love1ace <lovelacedud@gmail.com>
2024-07-29 11:05:31 +0900

    feature: 페이지 라우터 추가 했습니다 간단하게 #11

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+// src/App.tsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import PersonalWorkSchedule from './components/PersonalWorkSchedule';
+import PersonalWorkScheduleDetail from './components/PersonalWorkScheduleDetail';
+import WageVerification from './components/WageVerification';
+import WageVerificationDetail from './components/WageVerificationDetail';
+import WorkScheduleCorrectionRequest from './components/WorkScheduleCorrectionRequest';
+import WorkScheduleCorrectionRequestDetail from './components/WorkScheduleCorrectionRequestDetail';
+import MyProfile from './components/MyProfile';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/personal-work-schedule"
+          element={<PersonalWorkSchedule />}
+        />
+        <Route
+          path="/personal-work-schedule-detail"
+          element={<PersonalWorkScheduleDetail />}
+        />
+        <Route path="/wage-verification" element={<WageVerification />} />
+        <Route
+          path="/wage-verification-detail"
+          element={<WageVerificationDetail />}
+        />
+        <Route
+          path="/work-schedule-correction-request"
+          element={<WorkScheduleCorrectionRequest />}
+        />
+        <Route
+          path="/work-schedule-correction-request-detail"
+          element={<WorkScheduleCorrectionRequestDetail />}
+        />
+        <Route path="/my-profile" element={<MyProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
